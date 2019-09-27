aa11e7decea64aa4d4f4fe3198479e368e2cbf89
Sowmya Kumar <sowmyak@unr.edu>
2019-09-26 20:19:27 -0700

    double file

---
+++ double.py
@@ -0,0 +1,23 @@
+#function to take onr argument and make it double
+
+    
+def doublethisnow(first):
+    double = 2*first
+    print(double)
+    
+    
+doublethisnow(5)
+
+def doublethisgive(first):
+    return 2*first
+    
+    
+doubled = doublethisgive(5)
+print(doubled)
+
+
+def multiplythese(first):
+    return first*first
+
+ans = multiplythese(5)
+print(ans)
