ef72b5145721370ca3637c25c1b3775220d4438a
Sowmya Kumar <sowmyak@unr.edu>
2019-09-26 19:53:52 -0700

    sum

---
+++ sum.py
@@ -0,0 +1,24 @@
+# sum of two numbers
+import sys
+
+#arguments live in sys.argv
+#sys.argv =["sum.py", 3,5]
+
+if len(sys.argv) != 3:
+    print("Usage: python" + sys.argv[0] + " <first number><second number>")
+    sys.exit()
+    
+first = int(sys.argv[1])
+second= int(sys.argv[2])
+
+print("[0] and [1]".format(first, second))
+
+#result = first+second
+#print(result)
+
+
+
+def addthesetogether(a,b):
+    result = a+b
+    print(result)
+addthesetogether(first,second)
